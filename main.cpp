672ef80b368952b5bef9ac3398c3b0c9bfa27912
Chengxi (Chen) Shi <chengxis@usc.edu>
2015-11-01 14:29:55 -0800

    Create main.cpp
    Not a workable version for large input

---
+++ main.cpp
@@ -0,0 +1,93 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Codec {
+public:
+
+    const char VALID_NODE = 0;
+    const char NULL_NODE = 1;
+    
+    // Encodes a tree to a single string.
+    
+    string itoc(int val, int k){
+        int tmp = val;
+        string str = "";
+        for(int i = 0; i < k; i ++){
+            str = string(1, tmp % 256) + str;
+            tmp /= 256;
+        }
+        return str;
+    }
+    
+    int ctoi(string str){
+        int tmp = 0, ptr = 0;
+        while(ptr < str.length()){
+            tmp *= 256;
+            tmp += str[ptr++];
+        }//end while
+        return tmp;
+    }
+    
+    int serial_push(string *str, TreeNode* root){
+        int left_count = 0, right_count = 0;
+        if(root == NULL) return 0;
+        *str = *str + itoc(root -> val, 4);
+        int ptr = (*str).length();
+        *str = *str + string(8,0);
+        left_count = serial_push(str, root -> left);
+        right_count = serial_push(str, root -> right);
+        string lft_str = itoc(left_count, 4);
+        string rgt_str = itoc(right_count, 4);
+        (*str)[ptr] = lft_str[0];
+        (*str)[ptr+1] = lft_str[1];
+        (*str)[ptr+2] = lft_str[2];
+        (*str)[ptr+3] = lft_str[3];
+        (*str)[ptr+4] = rgt_str[0];
+        (*str)[ptr+5] = rgt_str[1];
+        (*str)[ptr+6] = rgt_str[2];
+        (*str)[ptr+7] = rgt_str[3];
+        return (left_count + right_count + 1);
+    }
+    
+    void deserial_build(string str, TreeNode* root){
+        root -> val = ctoi(str.substr(0,4));
+        
+        int leftct = ctoi(str.substr(4,4));
+        int rightct = ctoi(str.substr(8,4));
+       // root -> val += leftct + rightct;
+        if(leftct != 0){
+            root -> left = new TreeNode(0);
+            deserial_build(str.substr(12, 12 * leftct), root -> left);
+        }
+        if(rightct != 0){
+            root -> right = new TreeNode(0);
+            deserial_build(str.substr(12 + 12 * leftct, 12 * rightct), root -> right);
+        }
+    }
+    
+    string serialize(TreeNode* root) {
+        if(root == NULL) return "";
+        string mystr = "";
+        int tmp = serial_push(&mystr, root);
+        return mystr;
+    }
+
+    // Decodes your encoded data to tree.
+    TreeNode* deserialize(string data) {
+        TreeNode *root = new TreeNode(0);
+        //if(data.length() != 0) return root;
+        if(data.length() == 0) return NULL;
+        deserial_build(data, root);
+        return root;
+    }
+};
+
+// Your Codec object will be instantiated and called as such:
+// Codec codec;
+// codec.deserialize(codec.serialize(root));
